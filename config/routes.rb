ab2b4c7c1d6ddf88360dc721901458604898113b
Nicolas Oga <2112.oga@gmail.com>
2011-08-22 17:46:03 -0300

    COCO

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Xxi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => :games
+  root :to => "games#index"
 
   # See how all your routes lay out with "rake routes"
 
