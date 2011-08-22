e4c6483ca5f6529f7a0b943883a2c86e27b3daa0
Emmanuel Oga <EmmanuelOga@gmail.com>
2011-08-22 17:40:12 -0300

    Changed default route

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Xxi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => :games
 
   # See how all your routes lay out with "rake routes"
 
